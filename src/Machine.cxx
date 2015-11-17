4bbf126ac4e17e399ca019fa8e5358b3caf7bcc6
Richard B. Wagner <breckwagner@gmail.com>
2015-11-16 17:54:49 -0800

    Updated interface and machine after adding more methods to machine

--- src/Machine.cxx
+++ src/Machine.cxx
@@ -15,12 +15,39 @@ public:
 	Machine();
 
     /**
+     * Copy constructor for Machine
+     */
+    Machine(const Machine &obj);
+
+    /**
+     * Destructor for Machine
+     */
+    ~Machine();
+
+    /**
      * Changes the state of the machine to in use
      * @return true if successful in changing state to "in use" else false
      */
     bool start();
 
+    /**
+     * Changes the state of the machine to in use
+     * @return true if successful in changing state to "idle" else false
+     */
+    bool stop();
+
+    /**
+     * Compiles a report on the Machines use from the machine_log
+     * @return a vector with the machines use one item at a time chronologically
+     */
     vector<string> getReport();
+
+    /**
+     * Changes the id of the machine
+     * @param new_id is a new id to set the machines id to
+     * @return true if successful in changing id
+     */
+    bool setID(int);
 
 private:
     int id;
@@ -28,10 +55,19 @@ private:
     string status;
 };
 
-Machine::Machine() {
+Machine::Machine(void) {
     id = 0;
     status = "idle";
     machine_log.push_back(status);
+}
+
+Machine::Machine(const Machine &obj) {
+    id = 0;
+    status = "idle";
+    machine_log.push_back(status);
+}
+
+Machine::~Machine(void) {
 }
 
 bool Machine::start() {
@@ -42,6 +78,20 @@ bool Machine::start() {
     return true;
 }
 
+bool Machine::stop() {
+    if(status.compare("in use")==0) {
+        status = "idle";
+        machine_log.push_back(status);
+    } else return false;
+    return true;
+}
+
 vector<string> Machine::getReport() {
-    return "Machine " + id + ": " + machine_log;
+    return machine_log;
+}
+
+bool Machine::setID(int new_id) {
+    if(new_id>0) id = new_id;
+    else return false;
+    return true;
 }
--- src/interface.cxx
+++ src/interface.cxx
@@ -9,58 +9,26 @@
 
 using namespace std;
 
-bool process_cmd(std::string cmd) {
+bool process_cmd(std::string cmd, Machine m) {
     if(cmd.compare("quit")==0) {return false;}
     else if (cmd.find("help") == 0) {
         std::cout << "Options include: quit, help, status";
     }
     else if(cmd.compare("status")==0) {
-        
-        Machine x;
-        for (string i : x.machine_log ) {
+        for (string i : m.getReport() ) {
             cout << i;
         }
-        /*
-        for(auto it = machine_log.begin();
-                it != machine_log.end(); ++it){
-            cout << ", " << *it;
-        }*/
-        //std::cout << x.getReport();
     }
     else {std::cout << "unknown command\n";}
     return true;
 }
 
 int main(int argc, char *argv[]) {
+    Machine prototype;
     static const int maxchar = 5;
     std::string input;
 
     do {
         std::cout << "Type command: \n";
-    } while (std::cin >> input && process_cmd(input));
+    } while (std::cin >> input && process_cmd(input, prototype));
 }
-
-/*
-int main (int argc, char *argv[]) {
-	if (argc < 2) {
-		fprintf(stdout,"%s Version %d.%d\n",
-						argv[0],
-						Tutorial_VERSION_MAJOR,
-						Tutorial_VERSION_MINOR);
-		fprintf(stdout,"Usage: %s number\n",argv[0]);
-		return 1;
-	}
-
-	static const int maxchar = 5;
-    std::string nationname;
-
-    do {
-        std::cout << "What is the name of your nation?\n";
-    } while (std::cin >> nationname && nationname.size() > maxchar && show_error());
-
-	double inputValue = atof(argv[1]);
-	double outputValue = sqrt(inputValue);
-	fprintf(stdout,"The square root of %g is %g\n",
-					inputValue, outputValue);
-	return 0;
-}*/
